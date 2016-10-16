cc7b4e2bf8ccadbd885d22ba43d17e8dc352e5fa
JeanMenezes <jottamenezes@gmail.com>
2016-10-16 13:21:43 -0700

    Fixing problem on milestone 3
    Create screens and flow until milestone 6, but i’m having a problem
    with the segue methods of milestone 3

---
+++ dropBoxPrototype/SettingsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SettingsViewController.swift
+//  dropBoxPrototype
+//
+//  Created by user on 10/16/16.
+//  Copyright © 2016 Jean. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var settings: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.delegate = self
+        scrollView.contentSize = settings.frame.size
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ dropBoxPrototype/TermsServiceViewController.swift
@@ -0,0 +1,41 @@
+//
+//  TermsServiceViewController.swift
+//  dropBoxPrototype
+//
+//  Created by user on 10/16/16.
+//  Copyright © 2016 Jean. All rights reserved.
+//
+
+import UIKit
+
+class TermsServiceViewController: UIViewController {
+    @IBOutlet weak var doneTermsService: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func onDoneTermsService(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
