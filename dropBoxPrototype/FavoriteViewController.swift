eea775443d5955ddcfdb74c7c1fbd65ec148ab9e
JeanMenezes <jottamenezes@gmail.com>
2016-10-16 21:33:21 -0700

    Implemented button states

--- dropBoxPrototype/FavoriteViewController.swift
+++ dropBoxPrototype/FavoriteViewController.swift
@@ -9,9 +9,17 @@
 import UIKit
 
 class FavoriteViewController: UIViewController {
+    @IBOutlet weak var favoriteButton: UIButton!
 
+    @IBOutlet weak var favoritedIcon: UIImageView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        favoriteSelectedButton.isHidden = true
+        
+        favoritedIcon.isHidden = true
 
         // Do any additional setup after loading the view.
     }
@@ -20,7 +28,35 @@ class FavoriteViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    @IBAction func onFavorite(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        favoritedIcon.isHidden = false
+        
+        if favoriteButton.isSelected == false {
+            favoritedIcon.isHidden = true
+            
+            
+        }
+        
+//        if sender.isSelected {
+//        favoriteFileIcon.isHidden = false
+//            favoriteFileIcon.isSelected = !favoriteFileIcon.isSelected
+//        }
+        
+//        favoriteSelectedButton.isHidden = false
+//        favoriteFileIcon.isHidden = false
+//        favoriteButton.isHidden = true
+    }
     
+    @IBAction func onFavoriteSelected(_ sender: AnyObject) {
+//        favoriteSelectedButton.isHidden = true
+//        favoriteFileIcon.isHidden = true
+//        favoriteButton.isHidden = false
+//        print("favorite selected tapped")
+        
+        
+    }
 
     /*
     // MARK: - Navigation
